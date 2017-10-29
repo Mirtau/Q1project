4ab1f8f05854e7bd4afd98056b87f7c6e9c6709e
Mirtau <tjj72@comcast.net>
2017-10-29 15:58:55 -0600

    MVP!!!!!!!!!

--- exercise.js
+++ exercise.js
@@ -1,1 +1,15 @@
 var exercise = ['pushups', 'situps', 'plank', 'jumping jacks', 'wall sits', 'leg lifts', 'lunges', 'squats'];
+var selectedexercises = [];
+for (let i = 0; i < 5; i++) {
+  Array.prototype.exercisesplice = function() {
+    var ri = Math.floor(Math.random() * this.length);
+    var rs = this.splice(ri, 1);
+
+    return rs;
+
+  }
+  var result = exercise.exercisesplice();
+
+  selectedexercises.push(result);
+}
+document.getElementById('printedexercises').value = selectedexercises;
