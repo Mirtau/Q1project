b46fc9bf902fc278c8a2df9ab4549a643aa1efa9
Mirtau <tjj72@comcast.net>
2017-11-01 14:48:45 -0600

    mvp

--- src/exercise.js
+++ src/exercise.js
@@ -1,55 +1,56 @@
+function timer(str) {
+  return str
+}
+function namer(str){
+  localStorage.setItem('name', str);
+  var result = localStorage.getItem('name');
+  return result
+}
+function todoEx(exercise) {
+  let tempexer = exercise.slice();
+  let selectedexercises = [];
 
-function timer(str){
-  let input = $('#min');
-  input.val(str);
+  for (let i = 0; i < 5; i++) {
+    Array.prototype.exercisesplice = function() {
+      var ri = Math.floor(Math.random() * this.length);
+      var rs = this.splice(ri, 1);
+      return rs;
+    }
+    var result = tempexer.exercisesplice();
+    selectedexercises.push(result);
+  }
+  return selectedexercises
 }
 
-
 $(document).ready(function() {
-  $('#locstor').click(function(username){
+  $('#locstor').click(function(username) {
     event.preventDefault();
-    localStorage.setItem('name', $('#name').val());
-var result = localStorage.getItem('name');
-console.log(result);
-$('.title').append('Hello ' + result + ' ' + 'Welcome to PomoFit');
+    $('.title').append('Hello ' + namer($('#name').val()) + ' ' + 'Welcome to PomoFit');
 
-$('#name').val("");
+    $('#name').val("");
 
   })
-  $('.stbt').click(function(event) {
+  $('.btn').click(function(event) {
     event.preventDefault();
-    timer('25');
+    $('#min').val(timer(this.value));
   })
-  $('.exbt').click(function(event) {
-    event.preventDefault();
-    timer('5');
-  })
-  $('.clear').click(function(event) {
-    event.preventDefault();
-    timer('00');
-  })
+
   var exercise = ['pushups', 'situps', 'plank', 'jumping jacks', 'wall sits', 'leg lifts', 'lunges', 'squats', 'mtn climbers', 'crunches', 'side plank', 'wide hands push-ups'];
-  var selectedexercises = [];
   $('.ebutton').click(function(event) {
     event.preventDefault();
-    let tempexer = exercise.slice();
-    selectedexercises = [];
-
-    for (let i = 0; i < 5; i++) {
-      Array.prototype.exercisesplice = function() {
-        var ri = Math.floor(Math.random() * this.length);
-        var rs = this.splice(ri, 1);
-        return rs;
-      }
-      var result = tempexer.exercisesplice();
-      selectedexercises.push(result);
-    }
-    document.getElementById('printedexercises').value = selectedexercises;
+    // let tempexer = exercise.slice();
+    // let selectedexercises = [];
+    //
+    // for (let i = 0; i < 5; i++) {
+    //   Array.prototype.exercisesplice = function() {
+    //     var ri = Math.floor(Math.random() * this.length);
+    //     var rs = this.splice(ri, 1);
+    //     return rs;
+    //   }
+    //   var result = tempexer.exercisesplice();
+    //   selectedexercises.push(result);
+    // }
+    $('#printedexercises').val(todoEx(exercise));
 
   })
 })
-// module.exports = {
-// timer:timer,
-//
-//
-// }
--- test/tests.js
+++ test/tests.js
@@ -1,31 +1,20 @@
 const expect = chai.expect
 
 describe('timer', function() {
-  it('button puts time in clock', function() {
-    expect(timer).to.be.a('function');
-  })
-  it('should be a string', function(){
-    expect(timer('25')).to.equal('25')
+  it('returns the value of the button clicked', function() {
+    expect(timer(25)).to.equal(25);
   })
 })
 
-// describe('getsName', function() {
-//   it('gets user input', function() {
-//     expect(localStorage.getItem('name')).to.equal('Tim');
-//   })
-// })
-// describe('calculator', function () {
-//   it('is an object', function () {
-//     expect(calculator).to.be.a('object')
-//   })
-//
-//   describe('add', function () {
-//     it('should be a function', function () {
-//       expect(calculator.add).to.be.a('function')
-//     })
-//
-//     it('should add two numbers together', function () {
-//       expect(calculator.add(10,20)).to.equal(30)
-//     })
-//   })
-// })
+describe('namer', function() {
+  it('it puts name into welcome statement', function() {
+    expect(namer('')).to.equal('');
+  })
+})
+
+describe('todoEx', function() {
+  it('returns 5 random excercises', function() {
+    let array = ['pushups', 'situps', 'plank', 'jumping jacks', 'wall sits', 'leg lifts', 'lunges', 'squats', 'mtn climbers', 'crunches', 'side plank', 'wide hands push-ups'];
+    expect(todoEx(array).length).to.equal(5);
+  })
+})
